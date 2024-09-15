271b0cb87be90e89e5d989dde37d717885705a25
masum184e <masum184e@gmail.com>
2024-09-15 23:04:06 +0600

    updated

--- app/src/main/java/com/example/odyssey/MainActivity.java
+++ app/src/main/java/com/example/odyssey/MainActivity.java
@@ -3,27 +3,22 @@ package com.example.odyssey;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int SPLASH_DISPLAY_LENGTH = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent h = new Intent(MainActivity.this,SignupActivity.class);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
 
-                startActivity(h);
-            }
-        },3000);
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent mainIntent = new Intent(MainActivity.this, SigninActivity.class);
+            startActivity(mainIntent);
+            finish();
+        }, SPLASH_DISPLAY_LENGTH);
 
     }
 }
