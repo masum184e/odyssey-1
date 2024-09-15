35f7a15992308f6ed4c76975c8fa476db16a518c
imteaz1213 <mdimteaz567@gmail.com>
2024-09-15 11:54:00 +0600

    splash screen & signup page added

--- app/src/main/java/com/example/odyssey/MainActivity.java
+++ app/src/main/java/com/example/odyssey/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.odyssey;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,9 +15,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        Intent h = new Intent(MainActivity.this,SignupActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
 
+                startActivity(h);
+            }
+        },3000);
 
     }
 }
